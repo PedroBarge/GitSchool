d5c14594e364b06925a05dbfbabb823e48833d36
PedroBarge <pedro525tavares@gmail.com>
2023-11-01 15:07:03 +0000

    Pontos done
    piramide feita

--- Pontos/Pontos/src/Main.java
+++ Pontos/Pontos/src/Main.java
@@ -1,11 +1,38 @@
 public class Main {
     public static void main(String[] args) {
+        String points = "XXXXXXXXXXX";
+        int maxLines = 10;
         System.out.println("Pontos!");
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                f
+        System.out.println("\nDo menor para o maior");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(points.substring(i));
+        }
+        System.out.println("\nDo maior para o menor");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(points.substring(i));
+        }
+        System.out.println("\nPiramide");
+        for (int i = 0; i < maxLines; i++) {
+            for (int j = 0; j < maxLines - i; j++) {
+                System.out.print(" ");
             }
+            for (int k = 0; k <= i * 2; k++) {
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println("\nPiramide (Invertido)");
+        for (int i = maxLines ; i >= 0; i--) {
+            for (int j = 0; j < maxLines - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i * 2; k++) {
+                System.out.print("X");
+            }
+            System.out.println();
         }
     }
 }
